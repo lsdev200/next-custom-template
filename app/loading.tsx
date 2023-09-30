b2a37d48dcea2a41b7b566eaed8592bc661c48a4
lsdev200 <lsdev200@gmail.com>
2023-09-30 12:16:11 +0530

    30-9

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,13 +2,14 @@ import Image from "next/image";
 
 const Loading = () => {
   return (
-    <div className="fixed w-screen h-screen top-0 left-0 flex flex-col items-center justify-center bg-white bg-opacity-80 z-50">
+    <div className="fixed w-screen h-screen top-0 left-0 flex flex-col items-center justify-center bg-white bg-opacity-80 z-50 ">
       <div className="flex flex-col items-center space-y-4">
         <Image
           src="/assets/img/devonix.png"
           alt="Company Logo"
           width={270}
           height={270}
+          className="bg-blend-multiply"
         />
         <div className="text-gray-700 text-xl font-semibold animate-pulse">
           Re-Factoring Your Data
