4f4053eb9ef67869ad47f46e4b7c26ab40ff32c8
lsdev200 <lsdev200@gmail.com>
2023-12-21 11:38:27 +0530

    21-12-23

--- app/Dashboard/DashboardClient.tsx
+++ app/Dashboard/DashboardClient.tsx
@@ -1,5 +1,5 @@
-import Sidebar from "../components/Sidebar/Sidebar";
-import Navbar from "../components/Navbar/Navbar";
+// import Sidebar from "../components/Sidebar/Sidebar";
+// import Navbar from "../components/Navbar/Navbar";
 const DashboardClient = async () => {
   return (
     <div>
