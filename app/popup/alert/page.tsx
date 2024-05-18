79f8a7c4d0f0dbdf0b7760ce86af6a59de645a52
lsdev200 <lsdev200@gmail.com>
2024-05-18 10:52:04 +0530

    alert active

--- app/popup/alert/page.tsx
+++ app/popup/alert/page.tsx
@@ -6,11 +6,11 @@ const page = () => {
     <div className="w-full h-full flex flex-wrap justify-center items-center">
       <Alert type="success" message="Success alert message" />
 
-      {/* <Alert type="error" message="Error alert message" />
+      <Alert type="error" message="Error alert message" />
       <Alert type="info" message="Info alert message" />
       <Alert type="warning" message="Warning alert message" />
       <Alert type="primary" message="Primary alert message" />
-      <Alert type="secondary" message="Secondary alert message" /> */}
+      <Alert type="secondary" message="Secondary alert message" />
     </div>
   );
 };
